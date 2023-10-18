342256739a79c4d965e83cd5cbe1f090d70246a5
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-18 19:31:20 -0400

    building: recipe-tag.fake

---
+++ src/fakes/recipe-tag.fake.js
@@ -0,0 +1,22 @@
+// const { faker } = require('@faker-js/faker');
+// const { v4: uuidv4 } = require('uuid');
+
+
+// const generateOneTag = () => ({
+//   id: uuidv4(),
+//   name: faker.commerce.productMaterial(),
+//   createdAt: faker.date.anytime(),
+//   updatedAt: faker.date.anytime()
+// });
+
+// const generateManyTag = (size) => {
+//   const limit = size ?? 10;
+//   const fakeTag = [];
+
+//   for (let i = 0; i < limit; i += 1) {
+//     fakeTag.push(generateOneBook());
+//   }
+//   return [...fakeTag];
+// };
+
+// module.exports = { generateOneTag, generateManyTag };
