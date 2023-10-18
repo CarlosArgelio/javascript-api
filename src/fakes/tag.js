8590d724a09f17e4520df759b3485b0cb97b217f
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-18 19:13:44 -0400

    fix: import with ES6

--- src/fakes/tag.js
+++ src/fakes/tag.js
@@ -1,5 +1,5 @@
-import { faker } from '@faker-js/faker'
-import { v4 as uuidv4 } from 'uuid';
+const { faker } = require('@faker-js/faker');
+const { v4: uuidv4 } = require('uuid');
 
 
 const generateOneTag = () => ({
