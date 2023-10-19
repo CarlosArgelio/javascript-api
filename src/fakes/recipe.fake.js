954e50705bdce9f75b8239d35a6b0bbecce32a3d
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-19 07:26:17 -0400

    fix: add get ids to real in db to generate fake fata

--- src/fakes/recipe.fake.js
+++ src/fakes/recipe.fake.js
@@ -3,6 +3,8 @@ const { v4: uuidv4 } = require('uuid');
 
 const { models } = require('../../libs/sequelize');
 
+let categoryListId = [];
+let userListId = [];
 
 const findCategory = async () => {
   const categoryId = await models.Category.findAll()
@@ -14,18 +16,39 @@ const findUser = async () => {
   return userId
 };
 
-const generateOneRecipe = () => ({
-  id: uuidv4(),
-  categoryId: findCategory().then((resp) => { resp[0].dataValues.id }),
-  userId: findUser().then((resp) => { resp[0].dataValues.id }),
-  title: faker.person.jobTitle(),
-  description: faker.commerce.productDescription(),
-  ingredients: faker.commerce.productDescription(),
-  instructions: faker.commerce.productDescription(),
-  image: faker.image.url(),
-  createdAt: faker.date.anytime(),
-  updatedAt: faker.date.anytime()
-});
+const generateOneRecipe = async() => {
+  await findCategory()
+    .then( (categories) => {
+      for ( const category of categories ) {
+        const categoryId = category.dataValues.id;
+        categoryListId.push(categoryId)
+      };
+    });
+
+  await findUser()
+    .then( (users) => {
+      for ( const user of users ) {
+        const userId = user.dataValues.id;
+        userListId.push(userId)
+      };
+    });
+
+    const indexRandomCategory = Math.floor(Math.random() * categoryListId.length);
+    const indexRandomUSer = Math.floor(Math.random() * userListId.length);
+
+  return {
+    id: uuidv4(),
+    categoryId: categoryListId[indexRandomCategory],
+    userId: userListId[indexRandomUSer],
+    title: faker.person.jobTitle(),
+    description: faker.commerce.productDescription(),
+    ingredients: faker.commerce.productDescription(),
+    instructions: faker.commerce.productDescription(),
+    image: faker.image.url(),
+    createdAt: faker.date.anytime(),
+    updatedAt: faker.date.anytime()
+  }
+};
 
 const generateManyRecipe = (size) => {
   const limit = size ?? 10;
