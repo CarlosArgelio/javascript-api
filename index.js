8e60c6ac3aa59f46a006f4b2890a99210a68eb28
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-24 13:06:41 -0400

    feat: add routers to index

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const config = require('./config');
 const category = require('./components/category/network');
+const recipe = require('./components/recipe/network');
+const tag = require('./components/tag/network');
 
 const app = express();
 
 // ROUTERS
 app.use('/api/v1/categories', category);
+app.use('/api/v1/recipes', recipe);
+app.use('/api/v1/tags', tag);
 
 app.listen(config.api.port, () => {
   console.log('Listen API that port ', config.api.port);
