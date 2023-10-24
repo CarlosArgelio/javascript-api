be38c8be624617ce5ce9fa82a2554c7f2e85e706
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-10-24 14:56:24 -0400

    feat: add dependecy express-list-endpoints

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const listEndpoints = require('express-list-endpoints');
 const express = require('express');
 
 const config = require('./config');
@@ -7,11 +8,19 @@ const tag = require('./components/tag/network');
 
 const app = express();
 
-// ROUTERS
+// categories
 app.use('/api/v1/categories', category);
+
+// recipes
 app.use('/api/v1/recipes', recipe);
+
+// tags
 app.use('/api/v1/tags', tag);
+
+console.log(listEndpoints(app));
 
 app.listen(config.api.port, () => {
   console.log('Listen API that port ', config.api.port);
 });
+
+module.exports = app
